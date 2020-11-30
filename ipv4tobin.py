6814d1e624997baaaaff0cce6ce35f87ca83dbf4
Mario J <mjere003@gmail.com>
2020-11-30 18:21:33 -0500

    Update ipv4tobin.py

--- ipv4tobin.py
+++ ipv4tobin.py
@@ -11,7 +11,7 @@ for i in range(len(ipSplit)):
     binary.append(str(bin(int(ipSplit[i]))[2:].zfill(8))) # Takes split octet, conv to int, conv to bin() removes "0b"
                                                           # and fills with 0 up to 8 spaces with .zfill(8)
     brokenDown.append(ipSplit[i] + ' : ' + binary[i])     # Adds orig Octet w/Binary conv to list for breakdown per octet
-    #print(ipSplit[i], ' : ', binary[i])
+    #print(ipSplit[i], ' : ', binary[i])           # Uncomment to view loop values
 
 joinedBinary = ''.join(binary) # Need to display preceding zeros when converted to int()
 
